7a4f676544b8789fce7b0495e394e63ba253e3be
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-04-13 14:44:13 +0200

    2021-04-13 PJ

--- README.md
+++ README.md
@@ -5,18 +5,18 @@
 
 |Component|CSS selector|Variable name|Data type|
 |---------|------------|-------------|---------|
-|Opinion|div.user-post__card|opinion||
-|Opinion id|["data-entry-id"]|opinion_id||
-|Author|span.user-post__author-name|author||
-|Recomandation|span.user-post__author-recomendation > em|recommendation||
-|Star rating|span.user-post__score-count|stars||
-|Verfication|div.review-pz|verified||
-|Post date|span.user-post__published > time:nth-child(1)["datetime"]|post_date||
-|Purchase date|span.user-post__published > time:nth-child(2)["datetime"]|purchase_date||
-|Usefulness count|span[id^="votes-yes"]|usefulness||
-|Uselessness count|span[id^="votes-no"]|uselessness||
-|Content|div.user-post__text|content||
-|Advantages|div.review-feature__col:has(> div[class$="positives"] > div.review-feature__item|pros||
-|Disadvantages|div.review-feature__col:has(> div[class$="negatives"] > div.review-feature__item|cons||
+|Opinion|div.js__product_review|opinion|dict|
+|Opinion id|["data-entry-id"]|opinion_id|str|
+|Author|span.user-post__author-name|author|str|
+|Recomandation|span.user-post__author-recomendation > em|recommendation|bool|
+|Star rating|span.user-post__score-count|stars|float|
+|Verfication|div.review-pz|verified|str|
+|Post date|span.user-post__published > time:nth-child(1)["datetime"]|post_date|str|
+|Purchase date|span.user-post__published > time:nth-child(2)["datetime"]|purchase_date|str|
+|Usefulness count|span[id^="votes-yes"]|usefulness|int|
+|Uselessness count|span[id^="votes-no"]|uselessness|int|
+|Content|div.user-post__text|content|str|
+|Advantages|div.review-feature__col:has(> div[class$="positives"]) > div.review-feature__item|pros|list(str)|
+|Disadvantages|div.review-feature__col:has(> div[class$="negatives"]) > div.review-feature__item|cons|list(str)|
 3. extraction of single opinion 
 4. transformation of extracted data to given data types
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import re
+import json
 from bs4 import BeautifulSoup
 
 respons = requests.get("https://www.ceneo.pl/91715703#tab=reviews")
@@ -6,23 +8,51 @@ respons = requests.get("https://www.ceneo.pl/91715703#tab=reviews")
 page_dom = BeautifulSoup(respons.text,'html.parser')
 
 
-opinion = page_dom.select("div.user-post__card").pop(0)
+opinions = page_dom.select("div.js_product-review")
+all_opinions=[]
+for opinion in opinions:
+    opinon_id = opinion["data-entry-id"]
+    author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
+    try:
+        recomendation=opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
+        recomendation = True if recomendation=="Polecam" else False
+    except IndexError:
+        recomendation = None
+    stars=opinion.select("span.user-post__score-count").pop(0).get_text().strip()
+    stars = float(stars.split("/")[0].replace(",","."))
+    try:
+        verified=bool(opinion.select("div.review-pz").pop(0).get_text().strip())
+    except IndexError:
+        verified = False
+    post_date=opinion.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"].strip()
+    try:
+        purchase_date=opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
+    except IndexError:
+        purchase_date = None
+    usefulness=int(opinion.select("span[id^=\"votes-yes\"]").pop(0).get_text().strip())
+    uselessness=int(opinion.select("span[id^=\"votes-no\"]").pop(0).get_text().strip())
+    content=opinion.select("div.user-post__text").pop(0).get_text().strip()
+    content = re.sub("\\s"," ",content)
+    pros=opinion.select("div.review-feature__col:has(> div[class$=\"positives\"]) > div.review-feature__item")
+    pros =[item.get_text().strip() for item in pros]
+    cons=opinion.select("div.review-feature__col:has(> div[class$=\"negatives\"]) > div.review-feature__item")
+    cons =[item.get_text().strip() for item in cons]
+    single_opinon={
+        "opinon_id":opinon_id,
+        "author":author,
+        "recomendation":recomendation,
+        "stars":stars,
+        "content":content,
+        "pros":pros,
+        "cons":cons,
+        "verified":verified,
+        "post_date":post_date,
+        "purchase_date":purchase_date,
+        "usefulness":usefulness,
+        "uselessness":uselessness
+    }
 
-opinon_id = opinion["data-entry-id"]
-author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
-recomendation=opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
-stars=opinion.select("span.user-post__score-count").pop(0).get_text().strip()
-verified=opinion.select("div.review-pz").pop(0).get_text().strip()
-post_date=opinion.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"].strip()
-purchase_date=opinion.select("span.user-post__published > time:nth-child(2)[\"datetime\"]").pop(0)["datetime"].strip()
-usefulness=opinion.select("span[id^=\"votes-yes\"]").pop(0).get_text().strip()
-uselessness=opinion.select("span[id^=\"votes-no\"]").pop(0).get_text().strip()
-content=opinion.select("div.user-post__text").pop(0).get_text().strip()
-pros=opinion.select("div.review-feature__col:has(> div[class$=\"positives\"] > div.review-feature__item").pop(0).get_text().strip()
-pros =[item.get_text().strip() for item in pros]
-cons=opinion.select("div.review-feature__col:has(> div[class$=\"negatives\"] > div.review-feature__item").pop(0).get_text().strip()
-
-print (pros)
-#print(opinon_id, author, recomendation, stars, verified, stars, recomendation, stars)
-
+    all_opinions.append(single_opinon)
+print(json.dumps(all_opinions,ensure_ascii=False, indent=4))
+#print(opinon_id, author, recomendation, stars, verified, content)
 #print (author)
